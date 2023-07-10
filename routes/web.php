ca4edaad50f42831cdd943b61a825f2c06114f33
Eko Permana <permanaeko74@gmail.com>
2023-07-10 12:37:31 +0700

    Update Others

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\Auth\SocialAccountController;
 use App\Http\Controllers\Public\HomeController;
-use App\Http\Controllers\Scraping\IeeeController;
 use App\Http\Controllers\Scraping\SpringerController;
 use Illuminate\Support\Facades\Route;
 
