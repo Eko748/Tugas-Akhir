43caba3ed0abf844b063dd7efebb6871f34480d3
Eko Permana <permanaeko74@gmail.com>
2023-07-24 19:25:09 +0700

    update others

--- resources/views/public/pages/home/components/1-menu.blade.php
+++ resources/views/public/pages/home/components/1-menu.blade.php
@@ -8,12 +8,12 @@
                             <button class="btn btn-outline-primary" type="button" id="submit-review"><i
                                     class="fa fa-search"></i></button>
                             <input id="search" type="text" class="form-control"
-                                value="https://link.springer.com/chapter/10.1007/978-3-031-26281-4_3" aria-label="Example text with button addon"
+                                value="https://link.springer.com/chapter/10.1007/11774129_8" aria-label="Example text with button addon"
                                 aria-describedby="button-addon1">
                             <button class="btn btn-outline-primary" data-bs-trigger="hover" data-container="body"
                                 data-bs-toggle="popover" data-bs-placement="right" data-offset="-5px -5px"
                                 title="Tutorial Scraping"
-                                data-bs-content="Terapkan format URL pada kolom pencarian seperti: (https://link.springer.com/chapter/10.1007/978-3-031-26281-4_3)">
+                                data-bs-content="Terapkan format URL pada kolom pencarian seperti: (https://link.springer.com/chapter/10.1007/11774129_8)">
                                 <i class="fa fa-question-circle"></i>
                             </button>
                         </div>
